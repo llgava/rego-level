69bade3b63dcd03e378a3f924227c914f470c70e
llgava <llgavamt@gmail.com>
2022-07-14 23:48:00 -0400

    add: print for debug

--- regolevel/main.py
+++ regolevel/main.py
@@ -2,8 +2,9 @@ import utils.RegolithConfig as RegolithConfig
 import utils.BedrockPaths as BedrockPaths
 
 def main():
+  print('regolevel running...')
   profile = RegolithConfig.get_profile('build')
-  
+
   GAME_PATH = BedrockPaths.get_game_path()
   BedrockPaths.get_worlds(GAME_PATH)
 
