71f943673569a6bf61b44bde2bfe0abdaedff3a5
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-03-03 20:25:18 -0400

    feat: geckolib artifacts remover

--- rego-remover/core/remover.py
+++ rego-remover/core/remover.py
@@ -1,4 +1,5 @@
 import os
+import json
 import json5
 from glob import glob
 
@@ -9,17 +10,18 @@ def remove_texture_atlas(path: str):
     Removes Aseprite Texture Atlas configurations.
     This function only search files on specified path.
   """
+  print("Removing Aseprite atlas configurations...")
   json_files = glob(root_dir=path, pathname="**/*.json", recursive=True)
 
   for file in json_files:
     full_path = os.path.join(path, file)
     name = os.path.basename(full_path).split("/")[-1]
 
-    if (is_aseprite_texture_atlas(full_path)):
+    if (__is_aseprite_texture_atlas(full_path)):
       FileManager.delete_file(full_path)
       print("Aseprite Texture Atlas: " + name + " successfully removed.")
 
-def is_aseprite_texture_atlas(path: str) -> bool:
+def __is_aseprite_texture_atlas(path: str) -> bool:
   json_file = open(path, 'r')
   data = json5.loads(json_file.read())
 
@@ -28,10 +30,34 @@ def is_aseprite_texture_atlas(path: str) -> bool:
 
   return data["meta"]["app"] == "https://www.aseprite.org/"
 
+def remove_geckolib_artifacts(path: str) -> None:
+  """
+    Removes Geckolib animations objects.
+  """
+  print("Removing Geckolib animations artifacts...")
+  json_files = glob(root_dir=path, pathname="**/*.json", recursive=True)
+
+  for file in json_files:
+    full_path = os.path.join(path, file)
+    json_file = open(full_path, 'r')
+    data = json5.loads(json_file.read())
+    animations = data["animations"]
+
+    if ("geckolib_format_version" in data):
+      del data["geckolib_format_version"]
+
+    for anim in list(animations):
+      if (anim.endswith("_unbaked")):
+        del animations[anim]
+
+    with open(full_path, 'w') as anim_file:
+      json.dump(data, anim_file, indent=2)
+
 def remove_files_with_ext(extensions: list):
   """
     Removes files with specified extensions.
   """
+  print("Removing not allowed/required files...")
   files = glob(root_dir=".", pathname="**/*.*", recursive=True)
 
   for file in files:
@@ -41,7 +67,4 @@ def remove_files_with_ext(extensions: list):
 
     if (ext in extensions):
       full_path = path + ext
-      name = os.path.basename(full_path).split("/")[-1]
-
       FileManager.delete_file(full_path)
-      print("Extension not allowed/required: " + name + " successfully removed.")
--- rego-remover/main.py
+++ rego-remover/main.py
@@ -2,13 +2,14 @@ import os
 import core.remover as RemoveManager
 
 TEXTURES_FOLDER = os.path.join('RP', 'textures')
+ANIMATIONS_FOLDER = os.path.join('RP', 'animations')
 
 # Execute filter actions
 def main():
   RemoveManager.remove_texture_atlas(TEXTURES_FOLDER)
+  RemoveManager.remove_geckolib_artifacts(ANIMATIONS_FOLDER)
   RemoveManager.remove_files_with_ext([
-    ".ase", ".bbmodel", ".DS_Store",
-    ".txt", ".js", ".py", ".ts"
+    ".ase", ".bbmodel", ".DS_Store"
   ])
 
 if __name__ == '__main__':
