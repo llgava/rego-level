b33ffd2da47117439ce8222a31e0a6f91c64bf32
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-07-14 19:46:30 -0400

    test: regolith cache path

--- csharp-compiler/main.py
+++ csharp-compiler/main.py
@@ -5,9 +5,14 @@ import subprocess
 def main():
   this_path = os.path.realpath(__file__)
   filters_dir = os.path.dirname(this_path)
+  test_dir = os.path.join('..', '..', '..', filters_dir)
 
   print("Compiling C# filters...")
   print("")
+
+  print("THIS_PATH", this_path)
+  print("FILTERS_DIR", filters_dir)
+  print("TEST_DIR", test_dir)
 
 if __name__ == '__main__':
   main()
---
+++ csharp-compiler/utils/regolith.py
@@ -0,0 +1,5 @@
+class Regolith:
+
+  @staticmethod
+  def get_local_filters():
+    print("Hello world!")
