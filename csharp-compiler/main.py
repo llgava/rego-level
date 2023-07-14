ee0cf3fa11f4f4750a21db1d3663ea9b5fe862e6
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-07-14 19:41:44 -0400

    add: csharp-compiler directory; remove: rego-uuid project

---
+++ csharp-compiler/main.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import subprocess
+
+def main():
+  this_path = os.path.realpath(__file__)
+  filters_dir = os.path.dirname(this_path)
+
+  print("Compiling C# filters...")
+  print("")
+
+if __name__ == '__main__':
+  main()
--- rego-uuid/default_config.py
+++
@@ -1,7 +0,0 @@
-DEFAULT_CONFIGURATION = {
-  "target": "dev",
-  "name": "pack.name",
-  "description": "pack.description",
-  "version": "1.0.0",
-  "base_game_version": "1.20.0"
-}
