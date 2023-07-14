868723686a5f7ff72a21189f83fcd90bdbdb4398
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-07-14 19:54:58 -0400

    test: more regolith paths

--- csharp-compiler/main.py
+++ csharp-compiler/main.py
@@ -5,14 +5,17 @@ import subprocess
 def main():
   this_path = os.path.realpath(__file__)
   filters_dir = os.path.dirname(this_path)
-  test_dir = os.path.join('..', '..', '..', filters_dir)
+
+  parent_dir = os.path.dirname(path)
+  project_dir = os.path.abspath(os.path.join(parent, '..', '..', '..'))
 
   print("Compiling C# filters...")
   print("")
 
   print("THIS_PATH", this_path)
   print("FILTERS_DIR", filters_dir)
-  print("TEST_DIR", test_dir)
+  print("PARENT_DIR", parent_dir)
+  print("PROJECT_DIR", project_dir)
 
 if __name__ == '__main__':
   main()
