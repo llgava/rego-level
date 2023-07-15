f144de42fc0d96972974560c84a7ad1f5cf74ffe
Leonardo Luiz Gava <llgavamt@gmail.com>
2023-07-14 21:11:25 -0400

    fix: project root path

--- csharp-compiler/main.py
+++ csharp-compiler/main.py
@@ -6,7 +6,7 @@ from utils.regolith import *
 
 THIS_FILE_PATH = os.path.realpath(__file__)
 CSHARP_COMPILER_PATH = os.path.dirname(THIS_FILE_PATH)
-PROJECT_ROOT = os.path.abspath(os.path.join(CSHARP_COMPILER_PATH, '..', )) # os.path.abspath(os.path.join(CSHARP_COMPILER_PATH, '..', '..', '..', '..'))
+PROJECT_ROOT = os.path.abspath(os.path.join(CSHARP_COMPILER_PATH, '..', '..', '..', '..'))
 PROJECT_CONFIGURATION = os.path.join(PROJECT_ROOT, "config.json")
 
 def main():
